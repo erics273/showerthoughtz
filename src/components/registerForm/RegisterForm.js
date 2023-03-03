3d69d96a789dbef5349f77c3c088a04cb272c1f8
Patrick Justice <pjustice@caldwellschools.com>
2023-03-03 13:31:56 -0500

    Made RegisterForm a functional component

--- src/components/registerForm/RegisterForm.js
+++ src/components/registerForm/RegisterForm.js
@@ -1,73 +1,119 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 
 import { Redirect, withRouter, Link } from "react-router-dom";
+import { FormCheck } from "react-bootstrap";
 
-class RegisterForm extends Component {
-  state = {
-    errorMessage: null,
-    success: false,
-    formData: {
-      username: "",
-      fullName: "",
-      password: "",
-      confirmPassword: "",
-    },
+// class RegisterForm extends Component {
+//   state = {
+//     errorMessage: null,
+//     success: false,
+//     formData: {
+//       username: "",
+//       fullName: "",
+//       password: "",
+//       confirmPassword: "",
+//     },
+//   };
+
+//   handleChange = (event) => {
+//     let tempFormData = { ...this.state.formData };
+//     tempFormData[event.target.id] = event.target.value;
+//     this.setState({ formData: tempFormData });
+//   };
+
+//   handleSubmit = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       let response = await fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
+//         method: "POST", // or 'PUT'
+
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(this.state.formData),
+//       });
+//       console.log("resolved", response);
+
+//       if (response.status < 200 || response.status > 299) {
+//         throw Error(response.statusText);
+//       }
+//       this.setState({ success: true });
+
+//       let data = await response.json();
+//       console.log("new user created:", data);
+//     } catch (error) {
+//       console.error(error.message);
+//       this.setState({ errorMessage: "No! Do it AGAIN!!!" });
+//     }
+//   };
+// }
+
+  // ***********************
+
+  
+function RegisterForm() {
+  let [errorMessage, setErrorMessage] = useState(null);
+  let [success, setSuccess] = useState(false);
+  let [formData, setFormData] = useState({
+    username: "",
+    fullName: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  let handleChange = (event) => {
+    let { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
   };
 
-  handleChange = (event) => {
-    let tempFormData = { ...this.state.formData };
-    tempFormData[event.target.id] = event.target.value;
-    this.setState({ formData: tempFormData });
-  };
-
-  handleSubmit = async (event) => {
+  let handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       let response = await fetch(`${process.env.REACT_APP_API_URL}/api/users`, {
-        method: "POST", // or 'PUT'
-
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(this.state.formData),
+        body: JSON.stringify(formData),
       });
       console.log("resolved", response);
 
       if (response.status < 200 || response.status > 299) {
         throw Error(response.statusText);
       }
-      this.setState({ success: true });
+      setSuccess(true);
 
-      let data = await response.json();
+      const data = await response.json();
       console.log("new user created:", data);
     } catch (error) {
       console.error(error.message);
-      this.setState({ errorMessage: "No! Do it AGAIN!!!" });
+      setErrorMessage("No! Do it AGAIN!!!");
     }
   };
 
-  // ***********************
-  render() {
-    if (this.state.success) {
+
+
+    if (success) {
       return <Redirect to="/" />;
     }
     return (
       <div className="RegisterForm container">
-        {this.state.errorMessage && (
-          <Alert variant="danger">{this.state.errorMessage}</Alert>
+        {errorMessage && (
+          <Alert variant="danger">{errorMessage}</Alert>
         )}
 
         <h2 className="text-center">Come on in, the water is fine</h2>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId="username">
             <Form.Label>Create a Username</Form.Label>
             <Form.Control
-              onChange={this.handleChange}
-              value={this.state.formData.username}
+              onChange={handleChange}
+              value={formData.username}
               type="text"
               placeholder="Create a user name!"
             />
@@ -76,8 +122,8 @@ class RegisterForm extends Component {
           <Form.Group controlId="fullName">
             <Form.Label>Whats your full name?</Form.Label>
             <Form.Control
-              onChange={this.handleChange}
-              value={this.state.formData.fullName}
+              onChange={handleChange}
+              value={formData.fullName}
               type="text"
               placeholder="fullname!"
             />
@@ -86,8 +132,8 @@ class RegisterForm extends Component {
           <Form.Group controlId="password">
             <Form.Label>Create a Password</Form.Label>
             <Form.Control
-              onChange={this.handleChange}
-              value={this.state.formData.password}
+              onChange={handleChange}
+              value={formData.password}
               type="password"
               placeholder="Password"
             />
@@ -96,8 +142,8 @@ class RegisterForm extends Component {
           <Form.Group controlId="confirmPassword">
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
-              onChange={this.handleChange}
-              value={this.state.formData.confirmPassword}
+              onChange={handleChange}
+              value={formData.confirmPassword}
               type="password"
               placeholder="Confirm Password"
             />
@@ -112,9 +158,14 @@ class RegisterForm extends Component {
           </Button>
         </Form>
       </div>
-    );
-  }
-}
+    )
+    
+        }
+
+
+
+
+
 
 export default withRouter(RegisterForm);
 
