cce23b8f77744a7cccf49f838cf343fe8bb68004
Patrick Justice <pjustice@caldwellschools.com>
2023-04-25 16:09:06 -0400

    linked home icon with home page and did some minor styling to icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,12 @@
-import React, { useState} from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Alert from 'react-bootstrap/Alert';
-import { Link } from 'react-router-dom';
-import Showerdude from '../../logos/showerdude.jpg'
-import { FaHome } from 'react-icons/fa';
-import {BsFillChatRightTextFill} from "react-icons/bs";
-import {TiMessages} from "react-icons/ti";
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Alert from "react-bootstrap/Alert";
+import { Link } from "react-router-dom";
+import Showerdude from "../../logos/showerdude.jpg";
+import { FaHome } from "react-icons/fa";
+import { BsFillChatRightTextFill } from "react-icons/bs";
+import { TiMessages } from "react-icons/ti";
 
 import { connect } from "react-redux";
 import * as authActions from "../../redux/actions/auth";
@@ -15,25 +15,26 @@ import { bindActionCreators } from "redux";
 import APIService from "../../apiService";
 
 function Header(props) {
-
   let client = new APIService();
 
   const [errorMessage, setErrorMessage] = useState(null);
 
   let handleSignOut = (event) => {
-    client.logout(props.auth.token).then((response) => {
-      // handle success
-      localStorage.removeItem('auth');
-      props.actions.logout()
-    })
-      .catch((error) => {
-        console.log(error)
-        // handle error
-        setErrorMessage(error.response.data.message)
+    client
+      .logout(props.auth.token)
+      .then((response) => {
+        // handle success
+        localStorage.removeItem("auth");
+        props.actions.logout();
       })
-  }
+      .catch((error) => {
+        console.log(error);
+        // handle error
+        setErrorMessage(error.response.data.message);
+      });
+  };
 
-  let authLinks = ""
+  let authLinks = "";
   if (props.isAuthenticated) {
     authLinks = (
       <>
@@ -47,20 +48,49 @@ function Header(props) {
           </Nav>
         </Navbar.Collapse>
       </>
-    )
+    );
   }
-
 
   return (
     <div className="Navbar mb-3">
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand as={Link} style={{ fontFamily: 'Luckiest Guy',fontSize: '50pt' }}  to="/"><img src={Showerdude} width="150px" style={{marginRight: 20 }}/> ShowerThoughtz</Navbar.Brand>
-        <h3 className='homeIcon'>  <FaHome color="white" style={{ marginRight: '20px' }}  /></h3>
-        <h3 className='postIcon'> <BsFillChatRightTextFill color="white" style={{ marginRight: '20px' }} /> </h3> 
-       <h3 className='messageIcon'> <TiMessages  color="white" /> </h3>
+        <Navbar.Brand
+          as={Link}
+          style={{ fontFamily: "Luckiest Guy", fontSize: "50pt",  }}
+          to="/"
+        >
+          <img src={Showerdude} width="150px" style={{ marginRight: 20 }} />{" "}
+          ShowerThoughtz
+        </Navbar.Brand>
+        {/* ****homebutton**** */}
+
+        <Link to="/" className="homeIcon" style={{ fontSize: "3rem" }}>
+          <FaHome color="white" style={{marginBottom: '20px', marginRight: "20px" }} />
+        </Link>
+        {/* *********** */}
+        <h3 className="postIcon">
+          {" "}
+
+          <BsFillChatRightTextFill
+            color="white"
+            style={{ fontSize: "2rem", marginRight: "20px" }}
+          />{" "}
+        </h3>
+
+        <h3 className="messageIcon">
+          {" "}
+          <TiMessages color="white" 
+                    style={{ fontSize: "3rem", marginRight: "20px" }}
+                    />{" "}
+          
+        </h3>
         {authLinks}
       </Navbar>
-      {errorMessage && <div className="container mt-3"><Alert variant="danger">{errorMessage}</Alert></div>}
+      {errorMessage && (
+        <div className="container mt-3">
+          <Alert variant="danger">{errorMessage}</Alert>
+        </div>
+      )}
     </div>
   );
 }
@@ -73,17 +103,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(authActions, dispatch)
+    actions: bindActionCreators(authActions, dispatch),
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Header);
-
-
-
-
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
