63e22c0006d60d73459bfcdc612bc58348da0f77
Patrick Justice <pjustice@caldwellschools.com>
2023-05-16 16:17:27 -0400

    profile page is linked

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,38 +1,197 @@
-import moment from "moment";
-// import Button from 'react-bootstrap/Button'
-import React, { useState, useEffect } from "react";
+// import moment from "moment";
+// import React, { useState, useEffect } from "react";
+// import { generateAuthHeader, getUserGravatar } from "../../utils/authHelper";
+// import { getUserName } from "../../utils/authHelper";
+// import { Card, Button, Fade } from "react-bootstrap";
+// import md5 from "md5";
+// import { useParams } from "react-router-dom";
+// import { BsHandThumbsUp } from "react-icons/bs";
+// import { Link } from 'react-router-dom';
+// import Profile from "../../pages/Profile/Profile";
+
+// function Post(props) {
+
+//   const [showProfile, setShowProfile] = useState(false);
+//   let gravatarUrl = getUserGravatar(props.thoughtshit.username);
+
+//   // **********************Handle like button click**********************
+
+//   let handleLikeButton = async (event) => {
+//     event.preventDefault();
+
+//     try {
+//       const response = await fetch(
+//         `${process.env.REACT_APP_API_URL}/api/likes`,
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//             ...generateAuthHeader(),
+//           },
+//           body: JSON.stringify({
+//             postId: props.thoughtshit._id,
+//           }),
+//         }
+//       );
+//       if (response.status < 200 || response.status > 299) {
+//         throw Error(response.statusText);
+//       }
+//       console.log("post liked");
+
+//       // update liked state to true
+
+//       props.getPostsProp();
+//       // setLikedByCurrentUser(true);
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+
+//   // **********************Handle unlike button click**********************
+
+//   let handleUnlikeButtonClick = async (event) => {
+//     event.preventDefault();
+//     let currentUserName = getUserName();
+//     console.log(currentUserName);
+//     let likes = props.thoughtshit.likes;
+//     let likeId = null;
+
+//     for (let i = 0; i < likes.length; i++) {
+//       if (likes[i].username === currentUserName) {
+//         likeId = props.thoughtshit.likes[i]._id;
+
+//         // console.log("liked by", likes[i].username);
+//         // console.log("id:", likeId);
+//         // console.log(likes[i])
+//       }
+//     }
+
+//     if (!likeId) {
+//       console.log("User has not liked this post");
+//       return;
+//     }
+
+//     try {
+//       const response = await fetch(
+//         `${process.env.REACT_APP_API_URL}/api/likes/${likeId}`,
+//         {
+//           method: "DELETE",
+//           headers: {
+//             ...generateAuthHeader(),
+//           },
+//         }
+//       );
+//       if (response.status < 200 || response.status > 299) {
+//         throw Error(response.statusText);
+//       } else {
+//         console.log("post unliked");
+//       }
+//       props.getPostsProp();
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   };
+
+//   // displaying date
+//   let dateFormat = (postDate) => {
+//     let theDate = new Date(postDate);
+//     let theYear = theDate.getFullYear();
+//     let theMonth = theDate.getMonth() + 1;
+//     let theDay = theDate.getDate();
+
+//     // displaying the hours and days
+//     let currentTime = new Date();
+//     let diffInMs = currentTime - theDate;
+//     let diffInHours = Math.floor(diffInMs / 1000 / 60 / 60);
+//     let diffInDays = Math.floor(diffInHours / 24);
+//     let formattedDate = theMonth + "/" + theDay + "/" + theYear;
+
+//     if (diffInHours >= 1 && diffInHours < 24) {
+//       return `${diffInHours} hours ago`;
+//     } else if (diffInHours < 1) {
+//       return "Less than an hour ago";
+//     } else if (diffInDays === 1) {
+//       return "Yesterday";
+//     } else if (diffInDays <= 7) {
+//       return `${diffInDays} days ago`;
+//     } else {
+//       return formattedDate;
+//     }
+//   };
+
+//   let likedPost = props.thoughtshit.likes.some(
+//     (like) => like.username === getUserName()
+
+//   );
+
+//   const handleProfileClick = () => {
+//     setShowProfile(!showProfile);
+//     console.log("CHANGED")
+//   }
+
+//   return (
+
+//     <Card className="mb-3">
+//           <Card.Header style={{ backgroundColor: "wheat", fontFamily: "Luckiest Guy", fontSize: "20pt" }}>
+//        {props.thoughtshit.username}
+//        <Link to="/profile" onClick={handleProfileClick}>
+//               <img style={{ marginLeft: 10, borderRadius: '50%' }} src={gravatarUrl} alt="Profile picture" />
+//               </Link>
+//       </Card.Header>
+//       <Card.Body>
+//         {/* the users post */}
+//         <Card.Text style={{ fontWeight: "bold" }}>{props.thoughtshit.text}</Card.Text>
+//         <Card.Text style={{opacity:0.5 }} >
+//          {dateFormat(props.thoughtshit.createdAt)}
+//         </Card.Text>
+//         <div style={{ display: "inline-block" }}>
+//           <Button
+//             variant={likedPost ? "primary" : "secondary"}
+//             onClick={likedPost ? handleUnlikeButtonClick : handleLikeButton}
+//           >
+//             <BsHandThumbsUp style={{ marginRight: '5px' }} />
+//            {props.thoughtshit.likes.length}
+//           </Button>
+//         </div>
+//       </Card.Body>
+//            <Profile gravatarUrl={gravatarUrl} />
+//       {/* {showProfile && <Profile gravatarUrl={gravatarUrl} username={props.thoughtshit.username} />} */}
+//     </Card>
+
+//   );
+
+// }
+
+// export default Post;
+
+// *****Style*****
+
+// **********************/
+// Create a user profile page with the ability to update (edit profile)
+// On accessible by logging in
+// Use "create a user in the API"
+
+import React, { useState } from "react";
 import { generateAuthHeader, getUserGravatar } from "../../utils/authHelper";
 import { getUserName } from "../../utils/authHelper";
-import { Card, Button, Fade } from "react-bootstrap";
-import md5 from "md5";
-import { useParams } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
 import { BsHandThumbsUp } from "react-icons/bs";
-import { Link } from 'react-router-dom';
-// import { getUserGravatar } from "../../src/utils/authHelper";
-
-
+import { Link } from "react-router-dom";
+import Profile from "../../pages/Profile/Profile";
 
 function Post(props) {
-  let gravatarUrl = getUserGravatar(props.thoughtshit.username);
+  const [showProfile, setShowProfile] = useState(false);
+  const gravatarUrl = getUserGravatar(props.thoughtshit.username);
+  const likedPost = props.thoughtshit.likes.some(
+    (like) => like.username === getUserName()
+  );
 
+  const handleProfileClick = () => {
+    setShowProfile(!showProfile);
+    console.log("CHANGED");
+  };
 
-  // Like button
-  // const [likedByCurrentUser, setLikedByCurrentUser] = useState(false);
-
-  // let checkIfLikedByCurrentUser = () => {
-  //   let currentUserName = getUserName();
-  //   let likes = props.thoughtshit.likes;
-
-  //   for (let i = 0; i < likes.length; i++) {
-  //     if (likes[i].username === currentUserName) {
-  //       setLikedByCurrentUser(true);
-  //     }
-  //   }
-  // }
-
-  // **********************Handle like button click**********************
-
-  let handleLikeButton = async (event) => {
+  const handleLikeButton = async (event) => {
     event.preventDefault();
 
     try {
@@ -54,31 +213,22 @@ function Post(props) {
       }
       console.log("post liked");
 
-      // update liked state to true
-
       props.getPostsProp();
-      // setLikedByCurrentUser(true);
     } catch (error) {
       console.error(error.message);
     }
   };
 
-  // **********************Handle unlike button click**********************
-
-  let handleUnlikeButtonClick = async (event) => {
+  const handleUnlikeButtonClick = async (event) => {
     event.preventDefault();
-    let currentUserName = getUserName();
+    const currentUserName = getUserName();
     console.log(currentUserName);
-    let likes = props.thoughtshit.likes;
+    const likes = props.thoughtshit.likes;
     let likeId = null;
 
     for (let i = 0; i < likes.length; i++) {
       if (likes[i].username === currentUserName) {
-        likeId = props.thoughtshit.likes[i]._id;
-
-        // console.log("liked by", likes[i].username);
-        // console.log("id:", likeId);
-        // console.log(likes[i])
+        likeId = likes[i]._id;
       }
     }
 
@@ -108,19 +258,16 @@ function Post(props) {
     }
   };
 
-  // displaying date
-  let dateFormat = (postDate) => {
-    let theDate = new Date(postDate);
-    let theYear = theDate.getFullYear();
-    let theMonth = theDate.getMonth() + 1;
-    let theDay = theDate.getDate();
-
-    // displaying the hours and days
-    let currentTime = new Date();
-    let diffInMs = currentTime - theDate;
-    let diffInHours = Math.floor(diffInMs / 1000 / 60 / 60);
-    let diffInDays = Math.floor(diffInHours / 24);
-    let formattedDate = theMonth + "/" + theDay + "/" + theYear;
+  const dateFormat = (postDate) => {
+    const theDate = new Date(postDate);
+    const theYear = theDate.getFullYear();
+    const theMonth = theDate.getMonth() + 1;
+    const theDay = theDate.getDate();
+    const currentTime = new Date();
+    const diffInMs = currentTime - theDate;
+    const diffInHours = Math.floor(diffInMs / 1000 / 60 / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+    const formattedDate = `${theMonth}/${theDay}/${theYear}`;
 
     if (diffInHours >= 1 && diffInHours < 24) {
       return `${diffInHours} hours ago`;
@@ -134,69 +281,56 @@ function Post(props) {
       return formattedDate;
     }
   };
-  let likedPost = props.thoughtshit.likes.some(
-    (like) => like.username === getUserName()
-  );
 
   return (
-    // <div className="Post">
-    //     <div>
-    //         <div>Username: {props.thoughtshit.username}</div>
-
-    //         <div>Post: "{props.thoughtshit.text}"</div>
-
-    //         <div>Time Created: {dateFormat(props.thoughtshit.createdAt)}</div>
-
-    //         <div style={{ display: "inline-block" }}>
-    //                 {/* ^^ This is an if else statement but easier, "?" is the if and ":" is the else ^^*/}
-    //                 <Button variant={likedPost ? "primary" : "secondary"} onClick={likedPost ? handleUnlikeButtonClick : handleLikeButton}>
-
-    //                 Likes:{props.thoughtshit.likes.length}
-    //             </Button>
-    //         </div>
-    //         <div style={{ display: "inline-block" }}>
-    //         </div>
-    //     </div>
-
-    //     <hr />
-    // </div>
-
     <Card className="mb-3">
-          <Card.Header style={{ backgroundColor: "wheat", fontFamily: "Luckiest Guy", fontSize: "20pt" }}>
-       {props.thoughtshit.username}
-       <Link to="/profile">
-              <img style={{ marginLeft: 10, borderRadius: '50%' }} src={gravatarUrl} alt="Profile picture" />
-              </Link>
+      <Card.Header
+        style={{
+          backgroundColor: "wheat",
+          fontFamily: "Luckiest Guy",
+          fontSize: "20pt",
+        }}
+      >
+        {props.thoughtshit.username}
+        <Link to="/profile" onClick={handleProfileClick}>
+          <img
+            style={{ marginLeft: 10, borderRadius: "50%" }}
+            src={gravatarUrl}
+            alt="Profile picture"
+          />
+        </Link>
       </Card.Header>
       <Card.Body>
-        {/* the users post */}
-        <Card.Text style={{ fontWeight: "bold" }}>{props.thoughtshit.text}</Card.Text>
-        <Card.Text style={{opacity:0.5 }} >
-         {dateFormat(props.thoughtshit.createdAt)}
+        <Card.Text style={{ fontWeight: "bold" }}>
+          {props.thoughtshit.text}
+        </Card.Text>
+        <Card.Text style={{ opacity: 0.5 }}>
+          {dateFormat(props.thoughtshit.createdAt)}
         </Card.Text>
         <div style={{ display: "inline-block" }}>
-          <Button 
+          <Button
             variant={likedPost ? "primary" : "secondary"}
             onClick={likedPost ? handleUnlikeButtonClick : handleLikeButton}
           >
-            <BsHandThumbsUp style={{ marginRight: '5px' }} />
-           {props.thoughtshit.likes.length}
+            <BsHandThumbsUp style={{ marginRight: "5px" }} />
+            {props.thoughtshit.likes.length}
           </Button>
         </div>
       </Card.Body>
+      {showProfile && (
+        <Profile
+          gravatarUrlProp={gravatarUrl}
+          username={props.thoughtshit.username}
+        />
+      )}
+ 
+
     </Card>
+
+
   );
+ 
+
 }
 
-
 export default Post;
-
-// *****Style*****
-
-
-
-
-// **********************/
-// Create a user profile page with the ability to update (edit profile)
-// On accessible by logging in 
-// Use "create a user in the API"
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,35 +1,68 @@
-import React, { useState} from 'react';
+// import React, { useState } from "react";
+// import Header from "../../components/header/Header";
+// import { Container, Row, Col, Image, Button } from "react-bootstrap";
+
+// const Profile = ({props}) => {
+//   const gravatarUrl = props.gravatarUrlProp;
+//   console.log(gravatarUrl)
+
+//   return (
+//     <>
+//       <Header />
+
+//       <div className="d-flex justify-content-center">
+//         <Container className="mt-3" style={{ textAlign: "center" }}>
+//           <Row>
+//             <Col md={4}>
+//               <Image
+//               src= {(props.gravatarUrlProp)} roundedCircle
+//                 // src="https://via.placeholder.com/250" roundedCircle fluid
+//                 />
+//             </Col>
+//             <Col md={8}>
+//               {/* <h2>{props.thoughtshit.username}</h2> */}
+//               <p>This is a bio paaaage</p>
+//               <Button variant="primary">Edit Profile</Button>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Profile;
+
+// // get the users gravatar pic to show up in this profile.js component.
+
+import React from "react";
 import Header from "../../components/header/Header";
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
-import { getUserGravatar } from "../../utils/authHelper";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 
-const Profile = ({props}) => {
+const Profile = ({ username, gravatarUrlProp }) => {
+  console.log(username)
 
-  
-  
+
   return (
-
     <>
-    <Header />
-    
-    <div className="d-flex justify-content-center">
-      <Container className="mt-3" style={{ textAlign: 'center' }}>
-        <Row>
-          <Col md={4}>
-            <Image src="https://via.placeholder.com/250"  roundedCircle fluid />
-            {/* {props.getUserGravatar(props.username)} */}
-            
-          </Col>
-          <Col md={8}>
-            {/* <h2>{props.thoughtshit.username}</h2> */}
-            <p>This is a bio paaaage</p>
-            <Button variant="primary">Edit Profile</Button>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-</>
-);
+      <Header />
+
+      <div className="d-flex justify-content-center">
+        <Container className="mt-3" style={{ textAlign: "center" }}>
+          <Row>
+            <Col md={4}>
+              <Image src={gravatarUrlProp} roundedCircle fluid />
+            </Col>
+            <Col md={8}>
+              <h2>{username}</h2>
+              <p>This is a bio paaaaage</p>
+              <Button variant="primary">Edit Profile</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 };
 
-export default Profile
+export default Profile;
